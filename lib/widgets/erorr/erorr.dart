71b1326d09b609a5c7822fb57c61ca39b24419cf
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-03-30 20:42:26 +0430

    add erorr screans

--- lib/widgets/erorr/erorr.dart
+++ lib/widgets/erorr/erorr.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:shop/theme/constant.dart';
 
 class Erorr extends StatelessWidget {
   const Erorr({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Image.asset('assets/img/erorr.png'),
-        const Text('Sory !\nWe have Problems Here!'),
-        Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(Constans.padding),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('assets/gif/erorr.gif'),
+          Text(
+            'Sory !\nWe have Problems Here!',
+            style: Theme.of(context).textTheme.subtitle1,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            width: 200,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: kyellow,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text('Try Again'),
+                SizedBox(
+                  width: 8,
+                ),
+                Icon(Icons.refresh)
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
