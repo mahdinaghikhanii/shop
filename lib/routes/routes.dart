e77d549fe0d93671fe3fd049914bcc51d363be98
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-04-06 17:34:02 +0430

    add name routes page for Navigation

---
+++ lib/routes/routes.dart
@@ -0,0 +1,12 @@
+class RouteManager {
+  static const String cartViews = '/cartViews';
+  static const String detailViews = '/detailViews';
+  static const String favoriteViews = '/favoriteViews';
+  static const String helpViews = '/helpViews';
+  static const String homeViews = '/homeViews';
+  static const String loginViews = '/loginViews';
+  static const String onBoardViews = '/onBoardViews';
+  static const String searchViews = '/searchViews';
+  static const String settingViews = '/settingViews';
+  static const String regesterViews = '/regesterViews';
+}
