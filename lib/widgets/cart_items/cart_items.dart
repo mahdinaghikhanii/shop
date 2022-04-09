628bd3097a2ab0839991d32dee557fd3a5605330
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-04-09 22:15:11 +0430

    create in add to cart

--- lib/widgets/cart_items/cart_items.dart
+++ lib/widgets/cart_items/cart_items.dart
@@ -5,8 +5,8 @@ import 'package:shop/main.dart';
 import 'package:shop/provider/cart_provider/cart_provider.dart';
 import 'package:shop/constant.dart';
 import 'package:shop/view/detail/detail_views.dart';
-
-import '../buttons/add_remove_products_btn_cart/add_remove_products_btn_cart.dart';
+import '../../model/products_model/products_model.dart';
+import '../divider_defualt/divider_defualt.dart';
 
 class CartItems extends StatelessWidget {
   static const textPriceStyle = TextStyle(color: grey, fontSize: 14);
@@ -19,66 +19,134 @@ class CartItems extends StatelessWidget {
         fontSize: 14,
         fontWeight: FontWeight.w500);
     final size = MediaQuery.of(context).size;
-    return Consumer<CartProvider>(builder: (context, cart, child) {
+    return Consumer<CartProvider>(builder: (context, addcart, child) {
       context.watch<CartProvider>().getProdcutsCart();
       return Expanded(
         child: ListView.builder(
-          itemCount: cart.cartItems.length,
+          itemCount: addcart.cartItems.length,
           itemBuilder: (BuildContext context, int index) {
+            ProductsModel fav = addcart.cartItems[index];
             return GestureDetector(
               onTap: () {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
                         builder: (context) => DetailViews(
-                              productsModel: cart.cartItems[index],
+                              productsModel: addcart.cartItems[index],
                             )));
               },
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: Constans.padding,
-                    right: Constans.padding,
-                    top: Constans.padding,
-                    bottom: 5),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: size.width * 0.24,
-                      height: size.height * 0.16,
-                      child: CachedNetworkImage(
-                        imageUrl: cart.cartItems[index].image,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
+              child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: Constans.padding,
+                      right: Constans.padding,
+                      top: 10,
+                      bottom: 5),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(cart.cartItems[index].title,
-                              textAlign: TextAlign.left,
-                              maxLines: 2,
-                              style: textTitleStyle),
-                          const SizedBox(
-                            height: 8,
+                          SizedBox(
+                            width: size.width * 0.20,
+                            height: size.height * 0.16,
+                            child: CachedNetworkImage(
+                              imageUrl: fav.image,
+                            ),
                           ),
-                          Text("€ " + cart.cartItems[index].price.toString()),
-                          const SizedBox(
-                            height: Constans.smallSizedBox,
-                          ),
-                          AddRemoveProductsButtonCart(
-                            productsModel: cart.cartItems[index],
-                          )
                         ],
                       ),
-                    )
-                  ],
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(fav.title,
+                                textAlign: TextAlign.left,
+                                maxLines: 2,
+                                style: textTitleStyle),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 6, bottom: 8),
+                              child: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.star_rate,
+                                    size: Constans.smallIconSize,
+                                    color: kyellow,
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(fav.ratingModel.rate.toString()),
+                                ],
+                              ),
+                            ),
+                            Text("€ " + fav.price.toString()),
+                            const SizedBox(
+                              height: Constans.smallSizedBox,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: Constans.padding, right: Constans.padding),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        highlightColor: kyellow,
+                        borderRadius:
+                            BorderRadius.circular(Constans.smallBorderRadios),
+                        onTap: () {
+                          addcart.removeOneProductsCart(index);
+                        },
+                        child: Row(
+                          children: const [
+                            Icon(
+                              Icons.delete,
+                              color: grey,
+                              size: Constans.mediumsize,
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              'Remove cart',
+                              style: Constans.textStyleFavoriteAndCartGrayStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Spacer(),
+                      const Text(
+                        'View details',
+                        style: Constans.textStyleFavoriteViewAndBuy,
+                      ),
+                      const Icon(
+                        Icons.keyboard_arrow_right,
+                        color: kyellow,
+                        size: Constans.mediumsize,
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const DivderDefualt(),
+              ]),
             );
           },
         ),
