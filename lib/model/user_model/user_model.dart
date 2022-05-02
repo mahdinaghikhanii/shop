46c579a8c0b98b13fc498d110ddf60f0aa1e6514
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-03 01:33:49 +0430

    create in userModel

--- lib/model/user_model/user_model.dart
+++ lib/model/user_model/user_model.dart
@@ -1,7 +1,26 @@
+import 'package:appwrite/models.dart';
+
 class UserModel {
-  String userName = '';
-  String emailAddres = '';
-  String password = '';
-  String status = '';
-  bool islogin = false;
+  final String $id = '';
+
+  /// User name.
+  final String name = '';
+
+  /// User registration date in Unix timestamp.
+  final int registration = 0;
+
+  /// User status. 0 for Unactivated, 1 for active and 2 is blocked.
+  final int status = 0;
+
+  /// Unix timestamp of the most recent password update
+  final int passwordUpdate = 0;
+
+  /// User email address.
+  final String email = '';
+
+  /// Email verification status.
+  final bool emailVerification = false;
+
+  /// User preferences as a key-value object
+  late final Preferences prefs;
 }
