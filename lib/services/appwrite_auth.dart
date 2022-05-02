077928a064cb56b226f32f9cac0e21587e4db6a5
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-02 18:44:25 +0430

    create and add sharedperfarances for save login and username

--- lib/services/appwrite_auth.dart
+++ lib/services/appwrite_auth.dart
@@ -1,38 +1,55 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppwriteAuth extends ChangeNotifier {
-  // ignore: non_constant_identifier_names
-  final String LOGIN_STATUS = 'STATUSLOGIN';
+  //this part for connected appwrite Server
 
   Client client = Client()
       .setEndpoint("http://localhost:4003/v1") // Your API Endpoint
       .setProject("626a5244cf1dca4dee63") // Your project ID
       .setSelfSigned(status: true);
 
+  final String LOGIN_CHECK = 'LOGINCHECK';
+
+  final String LOGIN_USERNAME = 'LOGINUSERNAME';
+
   bool _isLoggedIn = false;
+  String _username = 'true';
 
   get getIsLogged => _isLoggedIn;
 
   void setsSaveSignInAndSignUp(bool check, String name) {
     _isLoggedIn = check;
+    _username = name;
     saveSignInAndSignUp(check, name);
     notifyListeners();
   }
 
   void saveSignInAndSignUp(bool check, String name) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    sharedPreferences.setBool(LOGIN_STATUS, check);
-    sharedPreferences.setString(LOGIN_STATUS, name);
+    await sharedPreferences.setBool(LOGIN_CHECK, check);
+    await sharedPreferences.setString(LOGIN_USERNAME, name);
+  }
+
+  getDarkThemeOrLightTheme() async {}
+
+  Future logOutAccount() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return await sharedPreferences.remove(LOGIN_CHECK);
   }
 
   getsaveSignInAndSignUp() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    if (sharedPreferences.getBool(LOGIN_STATUS) == null) {
+    if (sharedPreferences.getBool(LOGIN_CHECK) == null &&
+        sharedPreferences.getString(LOGIN_USERNAME) == '') {
       _isLoggedIn = false;
+      _username = '';
     } else {
-      _isLoggedIn = sharedPreferences.getBool(LOGIN_STATUS) ?? false;
+      _username = sharedPreferences.getString(LOGIN_USERNAME) ?? '';
+      _isLoggedIn = sharedPreferences.getBool(LOGIN_CHECK) ?? false;
     }
   }
 }
--- lib/view/setting/setting_views.dart
+++ lib/view/setting/setting_views.dart
@@ -18,191 +18,197 @@ class SettingViews extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     var multilanguage = S.of(context);
 
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Padding(
-            padding: const EdgeInsets.all(Constans.padding),
-            child: Text(
-              multilanguage
-                  .naemBtnNavigationInHomeViews_Setting_andAppbarSetting,
-              style: Theme.of(context).textTheme.subtitle1,
+    return WillPopScope(
+      onWillPop: () async {
+        ScaffoldMessenger.of(context);
+        return true;
+      },
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Padding(
+              padding: const EdgeInsets.all(Constans.padding),
+              child: Text(
+                multilanguage
+                    .naemBtnNavigationInHomeViews_Setting_andAppbarSetting,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
             ),
+            centerTitle: false,
+            titleSpacing: 0,
+            elevation: 0,
           ),
-          centerTitle: false,
-          titleSpacing: 0,
-          elevation: 0,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(Constans.padding),
-          child: SingleChildScrollView(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(multilanguage.setting_Text_Account,
-                          style: textTheme.subtitle1?.copyWith(fontSize: 18)),
-                      const SizedBox(
-                        height: 14,
-                      ),
-                      Container(
-                        height: 80,
-                        padding: const EdgeInsets.all(16),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: appProvider.brighness
-                                ? Colors.grey.shade400.withAlpha(100)
-                                : Colors.grey.shade200),
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 52,
-                              height: 52,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: appProvider.brighness
-                                      ? Colors.grey.shade700
-                                      : Colors.grey.shade300),
-                              child: Center(
-                                child: Icon(Icons.person,
-                                    size: 32, color: Colors.grey.shade500),
+          body: Padding(
+            padding: const EdgeInsets.all(Constans.padding),
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(multilanguage.setting_Text_Account,
+                            style: textTheme.subtitle1?.copyWith(fontSize: 18)),
+                        const SizedBox(
+                          height: 14,
+                        ),
+                        Container(
+                          height: 80,
+                          padding: const EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: appProvider.brighness
+                                  ? Colors.grey.shade400.withAlpha(100)
+                                  : Colors.grey.shade200),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 52,
+                                height: 52,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: appProvider.brighness
+                                        ? Colors.grey.shade700
+                                        : Colors.grey.shade300),
+                                child: Center(
+                                  child: Icon(Icons.person,
+                                      size: 32, color: Colors.grey.shade500),
+                                ),
                               ),
-                            ),
-                            const SizedBox(width: 16),
-                            Row(
-                              children: [
-                                InkWell(
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                        context, RouteManager.loginViews);
-                                  },
-                                  child: Text(
-                                      multilanguage.setting_button_login,
+                              const SizedBox(width: 16),
+                              Row(
+                                children: [
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                          context, RouteManager.loginViews);
+                                    },
+                                    child: Text(
+                                        multilanguage.setting_button_login,
+                                        style: textTheme.subtitle1?.copyWith(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w400,
+                                            color: kyellow)),
+                                  ),
+                                  Text(" / ",
                                       style: textTheme.subtitle1?.copyWith(
                                           fontSize: 18,
                                           fontWeight: FontWeight.w400,
                                           color: kyellow)),
-                                ),
-                                Text(" / ",
-                                    style: textTheme.subtitle1?.copyWith(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w400,
-                                        color: kyellow)),
-                                InkWell(
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                        context, RouteManager.signUp);
-                                  },
-                                  child: Text(
-                                      multilanguage.setting_button_register,
-                                      style: textTheme.subtitle1?.copyWith(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w400,
-                                          color: kyellow)),
-                                ),
-                              ],
-                            )
-                          ],
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                          context, RouteManager.signUp);
+                                    },
+                                    child: Text(
+                                        multilanguage.setting_button_register,
+                                        style: textTheme.subtitle1?.copyWith(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w400,
+                                            color: kyellow)),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        Text(
+                            multilanguage
+                                .naemBtnNavigationInHomeViews_Setting_andAppbarSetting,
+                            style: textTheme.subtitle1?.copyWith(fontSize: 18)),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        BuildListTile(
+                          ontap: () {
+                            showModalBottomSheet<void>(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return const AppearnaceBottomSheets();
+                                });
+                          },
+                          color: kpurple,
+                          icon: appProvider.brighness
+                              ? Icons.dark_mode
+                              : Icons.light_mode,
+                          title: multilanguage.setting_Listtitle_appearance,
+                          trailing: appProvider.brighness
+                              ? multilanguage.bottomsheet_btnText_dark
+                              : multilanguage.bottomsheet_btnText_light,
+                          visibilityArrowIcons: true,
+                        ),
+                        BuildListTile(
+                          ontap: () {
+                            showModalBottomSheet(
+                                context: context,
+                                builder: (context) =>
+                                    const LanguageBottomSheet());
+                          },
+                          color: korange,
+                          icon: Icons.language,
+                          title: multilanguage.setting_Listtile_Language,
+                          trailing: appProvider.language == const Locale("en")
+                              ? "English"
+                              : "فارسی",
+                          visibilityArrowIcons: true,
+                        ),
+                        BuildListTile(
+                          ontap: () {
+                            Navigator.pushNamed(
+                                context, RouteManager.favoriteViews);
+                          },
+                          color: kred,
+                          icon: Icons.favorite,
+                          title: multilanguage.setting_Listtile_Favorite,
+                          trailing: "",
+                          visibilityArrowIcons: true,
+                        ),
+                        BuildListTile(
+                          ontap: () {
+                            Navigator.of(context)
+                                .pushNamed(RouteManager.helpViews);
+                          },
+                          color: kgreen,
+                          icon: Icons.help,
+                          title: multilanguage.setting_Listtile_Help,
+                          trailing: "",
+                          visibilityArrowIcons: true,
+                        ),
+                        BuildListTile(
+                          ontap: () {},
+                          color: kpink,
+                          icon: Icons.logout,
+                          title: multilanguage.setting_Listtile_Logout,
+                          trailing: "",
+                          visibilityArrowIcons: true,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    InkWell(
+                      borderRadius: BorderRadius.circular(10),
+                      onTap: () async {
+                        await launch(
+                            'https://main--nostalgic-jennings-63dd2f.netlify.app/');
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          multilanguage.setting_linktext_aboutdeveloper,
+                          style: textTheme.labelMedium?.copyWith(fontSize: 14),
                         ),
                       ),
-                      const SizedBox(
-                        height: 25,
-                      ),
-                      Text(
-                          multilanguage
-                              .naemBtnNavigationInHomeViews_Setting_andAppbarSetting,
-                          style: textTheme.subtitle1?.copyWith(fontSize: 18)),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      BuildListTile(
-                        ontap: () {
-                          showModalBottomSheet<void>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return const AppearnaceBottomSheets();
-                              });
-                        },
-                        color: kpurple,
-                        icon: appProvider.brighness
-                            ? Icons.dark_mode
-                            : Icons.light_mode,
-                        title: multilanguage.setting_Listtitle_appearance,
-                        trailing: appProvider.brighness
-                            ? multilanguage.bottomsheet_btnText_dark
-                            : multilanguage.bottomsheet_btnText_light,
-                        visibilityArrowIcons: true,
-                      ),
-                      BuildListTile(
-                        ontap: () {
-                          showModalBottomSheet(
-                              context: context,
-                              builder: (context) =>
-                                  const LanguageBottomSheet());
-                        },
-                        color: korange,
-                        icon: Icons.language,
-                        title: multilanguage.setting_Listtile_Language,
-                        trailing: appProvider.language == const Locale("en")
-                            ? "English"
-                            : "فارسی",
-                        visibilityArrowIcons: true,
-                      ),
-                      BuildListTile(
-                        ontap: () {
-                          Navigator.pushNamed(
-                              context, RouteManager.favoriteViews);
-                        },
-                        color: kred,
-                        icon: Icons.favorite,
-                        title: multilanguage.setting_Listtile_Favorite,
-                        trailing: "",
-                        visibilityArrowIcons: true,
-                      ),
-                      BuildListTile(
-                        ontap: () {
-                          Navigator.of(context)
-                              .pushNamed(RouteManager.helpViews);
-                        },
-                        color: kgreen,
-                        icon: Icons.help,
-                        title: multilanguage.setting_Listtile_Help,
-                        trailing: "",
-                        visibilityArrowIcons: true,
-                      ),
-                      BuildListTile(
-                        ontap: () {},
-                        color: kpink,
-                        icon: Icons.logout,
-                        title: multilanguage.setting_Listtile_Logout,
-                        trailing: "",
-                        visibilityArrowIcons: true,
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  InkWell(
-                    borderRadius: BorderRadius.circular(10),
-                    onTap: () async {
-                      await launch(
-                          'https://main--nostalgic-jennings-63dd2f.netlify.app/');
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        multilanguage.setting_linktext_aboutdeveloper,
-                        style: textTheme.labelMedium?.copyWith(fontSize: 14),
-                      ),
-                    ),
-                  )
-                ]),
+                    )
+                  ]),
+            ),
           ),
         ),
       ),
