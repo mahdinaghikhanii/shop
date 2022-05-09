f5fd5ec15cba478c04836602a629bb99a08387ea
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-09 14:53:44 +0430

    add bloc folder in lib and create blocstate.dart and userbloc.dart in my bloc folder and add State in block State

---
+++ lib/bloc/blocstate.dart
@@ -0,0 +1,9 @@
+abstract class BlocState {}
+
+class Loading extends BlocState {}
+
+class Initial extends BlocState {}
+
+class Failed extends BlocState {}
+
+class Authenticated extends BlocState {}
